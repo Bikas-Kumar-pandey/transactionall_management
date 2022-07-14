6dc797714c30efad19456a371f31828469b9de85
bikas_kumar_pandey <bikaskumar494@gmail.com>
2022-07-14 10:44:22 +0530

    initial commit of project

---
+++ transaction-flight/src/main/java/com/transactional/TransactionFlightApplication.java
@@ -0,0 +1,13 @@
+package com.transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionFlightApplication.class, args);
+	}
+
+}
